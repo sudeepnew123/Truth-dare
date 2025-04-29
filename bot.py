5f4b1b15e5af6465a779484b5c5903c09040d9ec
sudeepnew123 <sudeepmandal8999@gmail.com>
2025-04-29 13:23:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,21 +7,51 @@ BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = int(os.environ["CHAT_ID"])
 
 truths = [
-    "Kya tumne kabhi kisi ko chhupkar stalk kiya hai?",
-    "Kya tum abhi kisi pe crush rakhte ho?",
-    "Kabhi kisi ko bina bataye kiss kiya hai?",
-    "Kya tumne kabhi girlfriend/boyfriend se jhooth bola hai?",
-    "Kis group member ko tum secretly pasand karte ho?"
+    "Tumhara sabse bada sapna kya hai jo abhi tak pura nahi hua?",
+    "Kya kabhi tumne apne teacher par crush kiya tha?",
+    "Agar koi tumhe propose kare to kya jawab doge?",
+    "Kabhi kisi ko apna fake naam bataya hai?",
+    "Apni life ka sabse awkward date yaad hai?",
+    "Tum apne crush ko secretly stalk karte ho?",
+    "Kabhi kisi ko 'I love you' galat samajh ke bol diya?",
+    "Agar time travel kar sakte, kis ex ya crush ke paas wapas jaate?",
+    "Kya tumhe apne dost ka secret pta hai jo usko nahi pata?",
+    "Apne kabhi kisi ko bina bataye follow/unfollow kiya hai?",
+    "Tum sabse zyada kisse jealous hote ho group me?",
+    "Apna sabse bada pyaar letter ya confession yaad hai?",
+    "Kabhi kisi ko bina matlab ke flirt kiya hai?",
+    "Agar abhi kiss karne ka mauka mile, kis group member ko select karoge?",
+    "Kya tum apne bestfriend se secret pyar karte the ya karti thi?",
+    "Agar ek din ke liye opposite gender ban jao to kya karoge?",
+    "Kabhi sapne me kisi group wale ko dekha hai?",
+    "Apna sabse bada guilty pleasure kya hai?",
+    "Tum apne crush ko kis naam se contact save karte ho?",
+    "Ek aisi baat jo tum sabse chhupate ho group me?"
 ]
 
-dares = [
-    "Group mein jispe crush hai usko propose karo (acting sahi honi chahiye!)",
-    "Ek ladka ya ladki ko pyaar bhara message bhejo",
-    "Apna funny selfie bhejo abhi!",
-    "Ek shayari ya song gao yahan sabke liye",
-    "Bina context ke 'I love you' kisi ko bhejo"
+truths = [
+    "Tumhara sabse bada sapna kya hai jo abhi tak pura nahi hua?",
+    "Kya kabhi tumne apne teacher par crush kiya tha?",
+    "Agar koi tumhe propose kare to kya jawab doge?",
+    "Kabhi kisi ko apna fake naam bataya hai?",
+    "Apni life ka sabse awkward date yaad hai?",
+    "Tum apne crush ko secretly stalk karte ho?",
+    "Kabhi kisi ko 'I love you' galat samajh ke bol diya?",
+    "Agar time travel kar sakte, kis ex ya crush ke paas wapas jaate?",
+    "Kya tumhe apne dost ka secret pta hai jo usko nahi pata?",
+    "Apne kabhi kisi ko bina bataye follow/unfollow kiya hai?",
+    "Tum sabse zyada kisse jealous hote ho group me?",
+    "Apna sabse bada pyaar letter ya confession yaad hai?",
+    "Kabhi kisi ko bina matlab ke flirt kiya hai?",
+    "Agar abhi kiss karne ka mauka mile, kis group member ko select karoge?",
+    "Kya tum apne bestfriend se secret pyar karte the ya karti thi?",
+    "Agar ek din ke liye opposite gender ban jao to kya karoge?",
+    "Kabhi sapne me kisi group wale ko dekha hai?",
+    "Apna sabse bada guilty pleasure kya hai?",
+    "Tum apne crush ko kis naam se contact save karte ho?",
+    "Ek aisi baat jo tum sabse chhupate ho group me?"
 ]
-
+    
 async def td(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message.chat.id != CHAT_ID:
         return
